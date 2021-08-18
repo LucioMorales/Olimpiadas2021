1f95d951b5492a43a31b900d73e2ac17b29c089a
Lucio Morales <lucio.moralesdemaria@gmail.com>
2021-08-18 17:44:00 -0300

    Store Model created

--- oniet/admin.py
+++ oniet/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class StoreAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Info', {
+          'fields': ('name','manager',)  
+        }),
+        ('Customer', {
+            'fields': ('currentAmount','maxCapacity',)
+        }),
+    )
+    list_display = ['id','name','manager','currentAmount','maxCapacity',]
+    list_display_links = ['name','manager','currentAmount','maxCapacity',]
+
+admin.site.register(Store,StoreAdmin)
--- oniet/models.py
+++ oniet/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=35)
+    manager = models.CharField(max_length=20)
+    maxCapacity = models.PositiveIntegerField()
+    currentAmount = models.PositiveIntegerField()
+    checkInTime = models.DateTimeField(auto_now_add=True)
+    checkOutTime = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Store'
+        verbose_name_plural = 'Stores'
+
+    def __str__(self):
+        return str(self.id)
