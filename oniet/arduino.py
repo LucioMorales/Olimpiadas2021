44e23ca7955276ab612255356c3925f504618c93
LucasPasolli <lippasolli@hotmail.com>
2021-08-23 06:40:24 -0300

    Functional Button Arduino

--- oniet/arduino.py
+++ oniet/arduino.py
@@ -1,19 +1,30 @@
 
 import pyfirmata
-import time
-
+import time 
 board = pyfirmata.Arduino('/dev/ttyACM0')
+
+
+
+button = board.digital[8]
+previous_button_state = 0
+##BUTTON_PIN = board.get_pin('d:8:i')
 
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-digital_input = board.get_pin('d:10:i')
-led = board.get_pin('d:13:o')
+button.mode = pyfirmata.INPUT
 
 while True:
-    sw = digital_input.read()
-    if sw is True:
-        led.write(1)
-    else:
-        led.write(0)
-    time.sleep(0.1)
+        # We run the loop at 100Hz
+        time.sleep(0.01)
+        
+        # Get button current state
+        button_state = button.read()
+        
+        # Check if button has been released
+        if button_state != previous_button_state:
+            if button_state == 0:
+                print("Button released")
+
+        previous_button_state = button_state
+
